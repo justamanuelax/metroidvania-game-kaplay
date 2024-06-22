1cdb7d5909570d9145167d1ea1c251b78bbf2f34
justamanuelax <justamanuelax@gmail.com>
2024-06-22 19:04:15 +0200

    First Commit

--- src/kaplayLoader.js
+++ src/kaplayLoader.js
@@ -1,5 +1,7 @@
 import kaboom from "../lib/kaboom.mjs";
 
+
+
 export const scale = 2;
 export const k = kaboom({
   width: 640 * scale,
@@ -9,9 +11,9 @@ export const k = kaboom({
   global: false,
 });
 
-k.loadFont("glyphmesss", "./assets/glyphmesss.ttf");
+k.loadFont("glyphmesss", "../assets/glyphmesss.ttf");
 
-k.loadSprite("player", "./assets/sprites/u.png", {
+k.loadSprite("player", "../assets/sprites/u.png", {
   sliceX: 8,
   sliceY: 9,
   anims: {
@@ -24,7 +26,7 @@ k.loadSprite("player", "./assets/sprites/u.png", {
   },
 });
 
-k.loadSprite("drone", "./assets/sprites/dr0ne.png", {
+k.loadSprite("drone", "../assets/sprites/dr0ne.png", {
   sliceX: 6,
   sliceY: 3,
   anims: {
@@ -34,7 +36,7 @@ k.loadSprite("drone", "./assets/sprites/dr0ne.png", {
   },
 });
 
-k.loadSprite("burner", "./assets/sprites/burn3r.png", {
+k.loadSprite("burner", "../assets/sprites/burn3r.png", {
   sliceX: 5,
   sliceY: 6,
   anims: {
@@ -47,7 +49,7 @@ k.loadSprite("burner", "./assets/sprites/burn3r.png", {
   },
 });
 
-k.loadSpriteAtlas("./assets/ui.png", {
+k.loadSpriteAtlas("../assets/ui.png", {
   healthBar: {
     x: 16,
     y: 16,
@@ -57,7 +59,7 @@ k.loadSpriteAtlas("./assets/ui.png", {
   },
 });
 
-k.loadSpriteAtlas("./assets/animations.png", {
+k.loadSpriteAtlas("../assets/animations.png", {
   cartridge: {
     x: 125,
     y: 145,
@@ -70,20 +72,20 @@ k.loadSpriteAtlas("./assets/animations.png", {
   },
 });
 
-k.loadSprite("tileset", "./assets/tileset.png", {
+k.loadSprite("tileset", "../assets/tileset.png", {
   sliceX: 33,
   sliceY: 21,
 });
 
-k.loadSprite("background", "./assets/background.png", {
+k.loadSprite("background", "../assets/background.png", {
   sliceX: 13,
   sliceY: 25,
 });
 
-k.loadSound("notify", "./assets/sounds/notify.mp3");
-k.loadSound("boom", "./assets/sounds/boom.wav");
-k.loadSound("health", "./assets/sounds/health.wav");
-k.loadSound("flamethrower", "./assets/sounds/flamethrower.mp3");
+k.loadSound("notify", "../assets/sounds/notify.mp3");
+k.loadSound("boom", "../assets/sounds/boom.wav");
+k.loadSound("health", "../assets/sounds/health.wav");
+k.loadSound("flamethrower", "../assets/sounds/flamethrower.mp3");
 
-k.loadSprite("room1", "./maps/room1.png");
-k.loadSprite("room2", "./maps/room2.png");
+k.loadSprite("room1", "../maps/room1.png");
+k.loadSprite("room2", "../maps/room2.png");
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { k } from "./kaplayLoader.js";
+
+k.scene("room1", () => {
+
+});
+
+k.scene("room2", () => {
+
+});
+
+k.scene("intro", () => {
+
+});
+
+k.go("intro");
+
+k.go("intro");
