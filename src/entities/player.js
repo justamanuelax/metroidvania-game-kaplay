8ef91cc86d4fe7fd82b877bc967f3108ffebe661
justamanuelax <justamanuelax@gmail.com>
2024-06-23 00:23:30 +0200

    Control Handlers Just in case

---
+++ src/entities/player.js
@@ -0,0 +1,71 @@
+import { k } from "../kaplayLoader";
+
+export function makePlayer(){
+    return k.make([
+        k.pos(), 
+        k.sprite("player"), k.area({
+         shape: new k.Rect(k.vec2(0,18), 12,12)   
+        }),
+        k.anchor("center"),
+        k.body({ 
+            mass: 100, 
+            jumpForce: 320
+        }),
+        k.doubleJump(state.current().isDoubleJumpUnlocked ? 2 : 1), 
+        k.opacity(), 
+        k.health(state.current().playerHp),
+        "player",
+        {
+            speed: 150, 
+            isAttacking: false, 
+            setControls(){ // 
+                this.controlHandlers = [];
+                
+
+                this.controlHandlers.push(k.keyPress( (key) => {
+                    if(key === "x"){
+                        if(this.curAnim() !== "jump") this.play("jump");
+                        this.doubleJump();
+                    }
+
+                    if(key === "z" && this.currAnim() !== "attack" && this.isGrounded()){
+                        this.isAttacking = true;
+                        this.add([ 
+                            k.pos(this.flipX ? -25 : 0, 10), k.area({ 
+                                shape: new k.Rect(k.vec2(0), 25, 10)
+                            }), 
+                            "sword-hitbox"
+                        ]);
+                        this.play("attack");
+
+                        this.onAnimEnd((anim) => { 
+                            if (anim === "attack"){
+                                const swordHitbox = k.get("sword-hitbox", 
+                                    {
+                                        recursive: true
+                                    }
+                                )[0];
+
+
+                                if(swordHitbox){
+                                    k.destroy(swordHitbox);
+                                    this.isAttacking = false;
+                                    this.play("idle");
+                                }
+
+                            }
+                        })
+
+                    }
+
+                }));
+
+
+                this.controlHandlers.push(k.keyPress( (key) => {
+
+                }));
+
+            }
+        }
+    ]);
+}
---
+++ src/state/globalStateManager.js
@@ -0,0 +1,29 @@
+export const StatePropsEnum = {
+    playerHp: "playerHP", 
+    isDoubleJumpUnlocked: "isDoubleJumpUnlocked",
+    playerInBossFight: "playerInBossFight",
+    isBossDefeated: "isBossDefeated"
+}
+
+function initStateManager() {
+
+    const state = {
+        playerHp: 3, 
+        maxPLayerHP: 3,
+        isDoubleJumpUnlocked: false, 
+        playerIsInBossFight: false, 
+        isBossDefeated: false
+    };
+
+    return {
+        current(){ 
+            return { ...state };
+        },
+        set(property, value){
+            state[property] = value;
+        }
+
+    }
+}
+
+export const state = initStateManager();
